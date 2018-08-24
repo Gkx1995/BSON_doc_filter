e36f014f09bddd0ce0cfc5c55c29c3d665984143
kai <kaixuan.gao@rubrik.com>
2018-08-24 11:51:52 -0700

    Support dot notation query

--- filter_generator.cpp
+++ filter_generator.cpp
@@ -408,7 +408,7 @@ bool Filter::should_insert(const bson_t* input_doc) {
             // check if input doc contains this dot field
             if (bson_iter_init(&doc_iter, input_doc) && bson_iter_find_descendant(&doc_iter, field.c_str(), &target_iter)) {
                 bson_iter_init(&filter_iter, filters.at(i));
-                std::cout << "target iter type: " << bson_iter_type(&target_iter) << std::endl;
+                std::cout << "target iter type: " << bson_iter_type(&target_iter) << "filter iter type: " << bson_iter_type(&filter_iter) << std::endl;
                 flag = filter_compare_elements(&target_iter, &filter_iter);
             } else {
                 flag = IGNORE_NUM;
