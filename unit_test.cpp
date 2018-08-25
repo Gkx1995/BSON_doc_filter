67e87aa04cef4a940de896693dd6af1ac27e0acc
kai <kaixuan.gao@rubrik.com>
2018-08-25 11:22:17 -0700

    BID-8389: Support dot notation projection and filter

--- unit_test.cpp
+++ unit_test.cpp
@@ -152,7 +152,7 @@ TEST_CASE("Test projections: select wrong_doc.a.b.c", "[get_input_doc_if_satisfi
     }
 }
 
-TEST_CASE("Test projections: select document.a.b.c,foo.bar.0.baz_0, int32", "[get_input_doc_if_satisfied_filter]") {
+TEST_CASE("Test projections: select document.a.b.c,foo.bar.0.baz_0,int32", "[get_input_doc_if_satisfied_filter]") {
     bson_t *input_doc = generate_fixed_input_doc();
 
     std::string q1 = "select document.a.b.c";
@@ -524,6 +524,37 @@ TEST_CASE( "input_doc have 11 data types", "[should_insert]" ) {
     }
 
     delete(input_doc);
+}
+
+TEST_CASE("Test projections: select document.a.b.c,foo.bar.0.baz_0,int32", "[get_input_doc_if_satisfied_filter]") {
+    bson_t *input_doc = generate_fixed_input_doc();
+
+    std::string q1 = "select document.a.b.c";
+    const bson_t* output_doc_1 = get_input_doc_if_satisfied_filter(input_doc, q1);
+    bson_t* valid_doc_1 = bson_new();
+    bson_t* a = bson_new();
+    bson_t* b = bson_new();
+    bson_t* c = bson_new();
+    BSON_APPEND_INT32(c, "c", 1);
+    BSON_APPEND_DOCUMENT(b, "b", c);
+    BSON_APPEND_DOCUMENT(a, "a", b);
+    BSON_APPEND_DOCUMENT(valid_doc_1, "document", a);
+
+    BSON_APPEND_INT32(valid_doc_1, "int32", 200);
+
+    CHECK(is_identical(output_doc_1, valid_doc_1) == true);
+
+    std::cout << "projection of input_doc: " << bson_as_json(output_doc_1, NULL) << std::endl;
+    std::cout << "projection of valid doc: " << bson_as_json(valid_doc_1, NULL) << std::endl;
+
+    if (input_doc == output_doc_1)
+        delete (input_doc);
+    else {
+        delete (output_doc_1);
+        delete (input_doc);
+    }
+    if (valid_doc_1)
+        delete (valid_doc_1);
 }
 
 
