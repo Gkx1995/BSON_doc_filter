143079e766233ed6f9809475c2b020ed5dc72933
kai <kaixuan.gao@rubrik.com>
2018-08-27 14:44:09 -0700

    BID-8389: Support dot notation projection and filter

--- main.cpp
+++ main.cpp
@@ -42,43 +42,43 @@ bson_t* generate_fixed_input_doc() {
 
 int main() {
 
-    generate_fixed_input_doc();
+//    generate_fixed_input_doc();
 
 //    query sth matched with input doc
-//    std::string query = "select foo.bar.1.baz_1";
-//
-//    // select all, should return 1
-////    std::string query = "where *";
-//
-//    // query sth not in input_doc, should return 0
-////    std::string query = "where string string <= 150";
-////    std::string query = "WHERE bool string = 1";
-////    std::string query = "WHERE string string = str";
-//
-//            auto* filter = new Filter(query);
-//
-//    bson_t* input_doc = filter->generate_input_doc();
-//
-////    filter->print_map();
-////    filter->print_filters();
-////    bool should_input = filter->should_insert(input_doc);
-//    const bson_t* projection = filter->get_input_doc_if_satisfied_filter(input_doc);
-//
-//
-//
-//    std::cout << "query is : \n" << query << std::endl;
-//
-//    std::cout << "input doc is:\n" << bson_as_json(input_doc, NULL) << std::endl;
-//
-////    std::cout << "input doc should be insert: " << should_input << std::endl;
-//    if (projection) {
-//        std::cout << "projection of input_doc: " << bson_as_json(projection, NULL) << std::endl;
-//    } else {
-//        std::cout << "do not exist such projection" << std::endl;
-//    }
-//
-////    delete(filter);
-//    delete(input_doc);
+    std::string query = "select foo.bar.1.baz_1";
+
+    // select all, should return 1
+//    std::string query = "where *";
+
+    // query sth not in input_doc, should return 0
+//    std::string query = "where string string <= 150";
+//    std::string query = "WHERE bool string = 1";
+//    std::string query = "WHERE string string = str";
+
+            auto* filter = new Filter(query);
+
+    bson_t* input_doc = filter->generate_input_doc();
+
+//    filter->print_map();
+//    filter->print_filters();
+//    bool should_input = filter->should_insert(input_doc);
+    const bson_t* projection = filter->get_input_doc_if_satisfied_filter(input_doc);
+
+
+
+    std::cout << "query is : \n" << query << std::endl;
+
+    std::cout << "input doc is:\n" << bson_as_json(input_doc, NULL) << std::endl;
+
+//    std::cout << "input doc should be insert: " << should_input << std::endl;
+    if (projection) {
+        std::cout << "projection of input_doc: " << bson_as_json(projection, NULL) << std::endl;
+    } else {
+        std::cout << "do not exist such projection" << std::endl;
+    }
+
+//    delete(filter);
+    delete(input_doc);
 
     return 0;
 }
